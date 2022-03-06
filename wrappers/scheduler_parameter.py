64d22247dc1c32bd19883b9236cd252437387a47
HugoSenetaire <hhjs@dtu.dk>
2022-03-06 21:00:22 +0100

    feat:change default scheduler for temeprature

--- wrappers/scheduler_parameter.py
+++ wrappers/scheduler_parameter.py
@@ -4,6 +4,6 @@
 
 
 ##### Scheduler parameters example :
-def regular_scheduler(temperature, epoch, cste = 0.999):
+def regular_scheduler(temperature, epoch, cste = 0.99999):
     return temperature * cste
 

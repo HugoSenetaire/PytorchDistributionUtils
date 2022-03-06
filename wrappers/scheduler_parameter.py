32d2ae4b137a7c45f7e1eb4db323cef063e6b037
HugoSenetaire <hhjs@dtu.dk>
2022-03-06 23:05:20 +0100

    change minimum of temeprature

--- wrappers/scheduler_parameter.py
+++ wrappers/scheduler_parameter.py
@@ -5,5 +5,7 @@
 
 ##### Scheduler parameters example :
 def regular_scheduler(temperature, epoch, cste = 0.99999):
+    if temperature < 1e-5 :
+        return temperature
     return temperature * cste
 

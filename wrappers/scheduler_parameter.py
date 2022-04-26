1fcb659fe0ed06502cb9d9e599aa3f13b1965581
HugoSenetaire <hhjs@dtu.dk>
2022-04-26 13:34:36 +0200

    Fix: change default min temp to 1e-2

--- wrappers/scheduler_parameter.py
+++ wrappers/scheduler_parameter.py
@@ -4,8 +4,8 @@
 
 
 ##### Scheduler parameters example :
-def regular_scheduler(temperature, epoch, cste = 0.99999):
-    if temperature < 1e-5 :
+def regular_scheduler(temperature, epoch, cste = 0.999):
+    if temperature < 1e-2 :
         return temperature
     return temperature * cste
 

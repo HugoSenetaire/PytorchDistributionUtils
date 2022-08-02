b4921a1ee4f477e5fbf016abca0ef403b74e32fb
HugoSenetaire <hhjs@dtu.dk>
2022-08-02 17:13:17 +0200

    fix:scheduler temperature

--- wrappers/scheduler_parameter.py
+++ wrappers/scheduler_parameter.py
@@ -1,10 +1,6 @@
 ##### Scheduler parameters example :
 def regular_scheduler(temperature, epoch, cste = 0.999):
-<<<<<<< HEAD
     if temperature < 1e-3 :
-=======
-    if temperature < 1e-2 :
->>>>>>> 1fcb659fe0ed06502cb9d9e599aa3f13b1965581
         return temperature
     return temperature * cste
 
